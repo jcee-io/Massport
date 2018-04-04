fce54349ea032354c97474f2750b27512c51cf82
Justin <jvcruz@ucdavis.edu>
2018-04-04 00:10:33 -0700

    added browser to index jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './Components/App';
  
+const Main = () => (
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+);
+
 document.addEventListener('DOMContentLoaded', function() {
-  ReactDOM.render(
-    <App/>,
-    document.getElementById('app')
-  );
+  ReactDOM.render(<Main />,document.getElementById('app'));
 });
