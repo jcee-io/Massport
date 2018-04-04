44d9ce7d0d240619cfcad5322d7dc25fd26b08f0
Justin <jvcruz@ucdavis.edu>
2018-04-03 23:53:17 -0700

    changed app into stateful component

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
 
-const App = () => (
-  <div>
-    <h1>hello world</h1>
-  </div>
-);
+class App extends Component {
+	render() {
+		return (
+		  <div>
+		    <h1>hello world</h1>
+		  </div>
+		);
+	}
+}
 
 export default App;
