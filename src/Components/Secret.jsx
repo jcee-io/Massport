9c5bd51a9ef511fe51625afb3a726471576d0155
Justin <jvcruz@ucdavis.edu>
2018-05-18 01:39:23 -0700

    create secret page component

---
+++ src/Components/Secret.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Secret extends Component {
+	render() {
+		return (
+		  <div>
+		    <h1>This is the secret page</h1>
+		  </div>
+		);
+	}
+}
+
+export default Secret;
--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './Components/App';
- 
+import Secret from './Components/Secret';
+
 const Main = () => (
   <BrowserRouter>
     <Switch>
     	<Route exact path="/" component={App} />
+      <Route path="/secret" component={Secret} />
     </Switch>
   </BrowserRouter>
 );
