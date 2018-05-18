3cd957e5998d173cf98f7b22a64863cca14d01fd
Justin <jvcruz@ucdavis.edu>
2018-05-18 04:30:44 -0700

    initialize cookie-session

--- server.js
+++ server.js
@@ -4,11 +4,19 @@ const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
 const path = require('path');
 const app = express();
- 
+const cookieSession = require('cookie-session');
+const { session } = require('./config/keys');
+const passport = require('passport');
+
 const compiler = webpack(webpackConfig);
- 
+
 app.use(express.static(__dirname + '/dist'));
- 
+
+app.use(cookieSession({
+	maxAve: 24 * 60 * 60 * 1000,
+	keys: [session.cookieKey]
+}));
+
 if(process.env.NODE_ENV !== 'production') {
   app.use(webpackDevMiddleware(compiler, {
     hot: true,
@@ -18,13 +26,13 @@ if(process.env.NODE_ENV !== 'production') {
       colors: true,
     },
     historyApiFallback: true,
-  }));  
+  }));
 }
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist/index.html'));
 });
- 
+
 const server = app.listen(3000, function() {
   const host = server.address().address;
   const port = server.address().port;
