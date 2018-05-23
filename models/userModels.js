e6db38c1093272ec6ac818515985cacc355bc536
Justin <jvcruz@ucdavis.edu>
2018-05-23 02:54:02 -0700

    add user schema

---
+++ models/userModels.js
@@ -0,0 +1,11 @@
+const mongoose = const  = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  username: String,
+  googleId: String
+});
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
