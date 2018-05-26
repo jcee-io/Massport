185bba2eee8c521c29262f56b0857b1399bcd002
Justin <jvcruz@ucdavis.edu>
2018-05-26 00:26:22 -0700

    add link to google auth

---
+++ src/Components/Login.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Login extends Component {
+	render() {
+		return (
+		  <div>
+		    <h1>This is the login page</h1>
+        <a href="/auth/google">Login With Google</a>
+		  </div>
+		);
+	}
+}
+
+export default Login;
