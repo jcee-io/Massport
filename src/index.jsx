f3e4a73da50dda10db553d0ab9701242b5141a54
Justin <jvcruz@ucdavis.edu>
2018-05-27 06:38:03 -0700

    added /auth/login route

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './Components/App';
 import Secret from './Components/Secret';
+import Login from './Components/Login';
 
 const Main = () => (
   <BrowserRouter>
     <Switch>
     	<Route exact path="/" component={App} />
-      <Route path="/secret" component={Secret} />
+      <Route path="/auth/login" component={Login} />
+      <Route path="/user" component={Secret} />
     </Switch>
   </BrowserRouter>
 );
