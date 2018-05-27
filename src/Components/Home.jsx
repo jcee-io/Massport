6a72a66566b9285cc2cd3226b36b542fdf8b21da
Justin <jvcruz@ucdavis.edu>
2018-05-27 15:10:00 -0700

    change App component into Home and create Header component

---
+++ src/Components/Home.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const Home = () => (
+	<div>
+		<h1>hello world</h1>
+	</div>
+);
+
+export default Home;
--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import App from './Components/App';
+import Home from './Components/Home';
 import Secret from './Components/Secret';
 import Login from './Components/Login';
 
-const Main = () => (
-  <BrowserRouter>
-    <Switch>
-    	<Route exact path="/" component={App} />
-      <Route path="/auth/login" component={Login} />
-      <Route path="/user" component={Secret} />
-    </Switch>
-  </BrowserRouter>
-);
+class Main extends Component {
+  render() {
+    return(
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/auth/login" component={Login} />
+          <Route path="/user" component={Secret} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(<Main />,document.getElementById('app'));
