0a776a0ee177e8ae93ef2e3c37b104fe148941c2
Justin <jvcruz@ucdavis.edu>
2018-06-07 17:33:50 -0700

    create template for keys file

---
+++ config/keys.template.js
@@ -0,0 +1,20 @@
+module.exports = {
+  google: {
+		clientID: 'CLIENT_ID',
+		clientSecret: 'CLIENT_SECRET'
+	},
+  facebook: {
+    clientID: 'CLIENT_ID',
+		clientSecret: 'CLIENT_SECRET'
+  },
+  github: {
+    clientID: 'CLIENT_ID',
+		clientSecret: 'CLIENT_SECRET'
+  },
+  mongodb: {
+    dbURI: 'DB_URL'
+  },
+  session: {
+    cookieKey: 'COOKIE_KEY'
+  }
+}
